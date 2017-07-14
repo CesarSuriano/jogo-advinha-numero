6ef2794e5c25b4d6db591794e6d23c83091bb755
César Augusto <cesaraugustokg6@hotmail.com>
2017-07-13 23:15:46 -0300

    Arrumando uns bug

--- Onsen UI/www/js/app.js
+++ Onsen UI/www/js/app.js
@@ -1,4 +1,5 @@
 let contador = 0;
+let numComponentes = 0;
 
 document.addEventListener('init', function (event) {
     let page = event.target;
@@ -9,8 +10,9 @@ document.addEventListener('init', function (event) {
         };
     }
 
-    if (page.id === 'page8') {
-        document.getElementById("resultado").innerHTML = "Seu número é: " + contador;
+    else if (page.id === 'page8') {
+        document.getElementsByClassName("resultado")[numComponentes].innerHTML = "Seu número é: " + contador;
+        numComponentes ++;
     }
 });
 
@@ -18,4 +20,9 @@ function click_resposta(value, page) {
     contador += value;
     console.log(contador)
     document.querySelector('#myNavigator').pushPage(page + '.html', { data: { title: '' } });
+}
+
+function recomecar() {
+    contador = 0;
+    document.querySelector('#myNavigator').pushPage('page2.html', { data: { title: 'Page 2' } });
 }
